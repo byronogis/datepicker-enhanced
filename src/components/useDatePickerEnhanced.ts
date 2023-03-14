6e2b8e91cbfb79e5c9e1225c19af7eb03c53ec8b
Byron <byronogis@outlook.com>
2023-03-14 22:39:18 +0800

    fix: new value will change when change the panel

--- src/components/useDatePickerEnhanced.ts
+++ src/components/useDatePickerEnhanced.ts
@@ -167,18 +167,16 @@ export default function useDatePickerEnhanced(
           ? 0
           : 1
 
-      const currentSelectValue = [item.year, item[props.type] || 0]
-
       // 第二次点击和第一次点击的值相同时, 不做后续处理
-      if (itemClickTimes === 2 && (Number(preUpdateModelValue[lastClickIndex].join('')) === Number(currentSelectValue.join('')))) {
+      if (itemClickTimes === 2 && (Number(preUpdateModelValue[lastClickIndex].join('')) === Number([item.year, item[props.type] || 0].join('')))) {
         // 有效点击次数 - 1
         itemClickTimes -= 1
         return
       }
 
       lastClickIndex = index
-      panelValue.value[target] = currentSelectValue
-      preUpdateModelValue[target] = currentSelectValue
+      panelValue.value[target] = [item.year, item[props.type] || 0]
+      preUpdateModelValue[target] = [item.year, item[props.type] || 0]
     }
   }
   const panelTitleClick = (index: number) => {
